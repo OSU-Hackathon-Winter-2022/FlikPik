b46427b16c5488e68ed15ae9a76f76dd0cc2f6ba
hakkilab <blake.austin.hakkila@gmail.com>
2022-01-06 17:14:38 -0800

    Basic movie profile swiping component

---
+++ components/MovieProfileView.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+type MovieProps = {
+    title: string;
+    year: string;
+    coverImageURL: string;
+    runtime: string;
+    directors: string;
+    writers: string;
+    stars: string;
+    genres: string;
+    rating: string;
+    score: string;
+    id: string;
+    style: Object;
+};
+
+export type MovieProperties = MovieProps;
+
+export function MovieProfileView(props : MovieProps) {
+    let url = props.coverImageURL
+    return (
+        <View style={props.style}>
+            <Image source={{uri: url}} style={{resizeMode: 'contain', width: 315, height: 400}} />
+            <Text>Title: {props.title}</Text>
+            <Text>Release Year: {props.year}</Text>
+            <Text>Rating: {props.rating}</Text>
+            <Text>Runtime: {props.runtime}</Text>
+            <Text>Genre(s): {props.genres}</Text>
+            <Text>Starring: {props.stars}</Text>
+            <Text>IMDB Score: {props.score}</Text>
+        </View>
+    );
+}
---
+++ components/MovieSwiper.tsx
@@ -0,0 +1,167 @@
+import React, { Component, useState } from 'react'
+import Swiper from 'react-native-deck-swiper'
+import { StyleSheet, View } from 'react-native'
+import { MovieProperties, MovieProfileView } from './MovieProfileView'
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F5FCFF'
+    },
+    movie: {
+      flex: 1,
+      borderRadius: 4,
+      borderWidth: 2,
+      borderColor: '#E8E8E8',
+      justifyContent: 'center',
+      backgroundColor: 'white'
+    },
+    text: {
+      textAlign: 'center',
+      fontSize: 50,
+      backgroundColor: 'transparent'
+    },
+    done: {
+      textAlign: 'center',
+      fontSize: 30,
+      color: 'white',
+      backgroundColor: 'transparent'
+    }
+  })
+
+type SwiperProps = {
+    movies: Array<Object>
+}
+
+type SwiperState = {
+    movies: Array<Object>,
+    swipedAllMovies: Boolean,
+    swipeDirection: string,
+    movieIndex: number
+}
+
+export class MovieSwiper extends Component<SwiperProps, SwiperState> {
+    constructor (props) {
+      super(props)
+      this.state = {
+        movies: props.movies,
+        swipedAllMovies: false,
+        swipeDirection: '',
+        movieIndex: 0
+      }
+    }
+  
+    renderMovieProfile = (movie: MovieProperties, index: number) => {
+        return (
+            <MovieProfileView style={styles.movie} {...movie} />
+        )
+    };
+  
+    onSwiped = (type) => {
+      console.log(`on swiped ${type}`)
+    }
+  
+    onSwipedAllMovies = () => {
+      this.setState({
+        swipedAllMovies: true
+      })
+    };
+  
+    render () {
+      return (
+        <View style={styles.container}>
+          <Swiper
+            ref={swiper => {
+              this.swiper = swiper
+            }}
+            onSwiped={() => this.onSwiped('general')}
+            onSwipedLeft={() => this.onSwiped('left')}
+            onSwipedRight={() => this.onSwiped('right')}
+            onSwipedTop={() => this.onSwiped('top')}
+            onSwipedBottom={() => this.onSwiped('bottom')}
+            cards={this.state.movies}
+            cardIndex={this.state.movieIndex}
+            cardVerticalMargin={80}
+            renderCard={this.renderMovieProfile}
+            onSwipedAll={this.onSwipedAllMovies}
+            stackSize={3}
+            stackSeparation={15}
+            overlayLabels={{
+              bottom: {
+                title: 'SKIP',
+                style: {
+                  label: {
+                    backgroundColor: 'black',
+                    borderColor: 'black',
+                    color: 'white',
+                    borderWidth: 1
+                  },
+                  wrapper: {
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }
+                }
+              },
+              left: {
+                title: 'NOPE',
+                style: {
+                  label: {
+                    backgroundColor: 'black',
+                    borderColor: 'black',
+                    color: 'white',
+                    borderWidth: 1
+                  },
+                  wrapper: {
+                    flexDirection: 'column',
+                    alignItems: 'flex-end',
+                    justifyContent: 'flex-start',
+                    marginTop: 30,
+                    marginLeft: -30
+                  }
+                }
+              },
+              right: {
+                title: 'LIKE',
+                style: {
+                  label: {
+                    backgroundColor: 'black',
+                    borderColor: 'black',
+                    color: 'white',
+                    borderWidth: 1
+                  },
+                  wrapper: {
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    justifyContent: 'flex-start',
+                    marginTop: 30,
+                    marginLeft: 30
+                  }
+                }
+              },
+              top: {
+                title: 'SUPER LIKE',
+                style: {
+                  label: {
+                    backgroundColor: 'black',
+                    borderColor: 'black',
+                    color: 'white',
+                    borderWidth: 1
+                  },
+                  wrapper: {
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }
+                }
+              }
+            }}
+            animateOverlayLabelsOpacity
+            animateCardOpacity
+            swipeBackCard
+          >
+          </Swiper>
+        </View>
+      )
+    }
+  }
