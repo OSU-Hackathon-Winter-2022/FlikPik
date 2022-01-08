54583cf628a4766fee8a5ef17cd746cbda79f2d3
hakkilab <blake.austin.hakkila@gmail.com>
2022-01-08 06:37:22 -0800

    Modified swipe to be able to load new profiles and created recommendation code

--- screens/SwipeScreen/SwipeScreen.tsx
+++ screens/SwipeScreen/SwipeScreen.tsx
@@ -3,41 +3,8 @@ import { StyleSheet, View } from 'react-native';
 import { MovieSwiper } from '../../components/MovieSwiper';
 import top_movies from '../../database/top_250_by_rating.json'
 import { useNavigation } from '@react-navigation/native';
+import { getRandomMovieList } from '../../recommendation_engine/Recommender'
 
-let topMovieProfiles = [];
-for (let movie of top_movies.results) {
-    let movieProfile = {
-        id: movie.id,
-        title: movie.title,
-        year: movie.description.slice(-5, -1),
-        runtime: movie.runtimeStr,
-        coverImageURL: movie.image,
-        stars: movie.stars,
-        genres: movie.genres,
-        rating: movie.contentRating,
-        score: movie.imDbRating,
-        plot: movie.plot
-    }
-    topMovieProfiles.push(movieProfile);
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
-let selected = new Set();
-let default_movies = [];
-let i = 0;
-while(i < 10) {
-    let randIndex = getRandomInt(0, topMovieProfiles.length);
-    if (!selected.has(randIndex)) {
-        default_movies.push(topMovieProfiles[randIndex]);
-        selected.add(randIndex);
-        i++;
-    }
-}
 
 const Swipe = () => {
     const navigation = useNavigation();
@@ -47,7 +14,7 @@ const Swipe = () => {
     return (
 
         <View>
-            <MovieSwiper movies={default_movies} />
+            <MovieSwiper movies={getRandomMovieList(10)} />
         </View>
 
 
